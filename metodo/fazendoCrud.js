2c2a8d4f0bcb8996959606b70577b7c345091659
jonathan_stoner <jony5851@gmail.com>
2026-05-06 21:52:51 -0300

    Add PUT and DELETE methods for livros endpoint

--- metodo/fazendoCrud.js
+++ metodo/fazendoCrud.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const { json } = require('stream/consumers');
+
 
 let livros = [
     {
         id: 1,
-        titulo: "pequeno principe",
+        descricao: "pequeno principe",
         autor: "antonie de saint -exupery"
     },
     {
@@ -77,6 +77,72 @@ const servidor = http.createServer((req, res) => {
 
 
     }
+    // metodo put
+    //
+    if (url == "/livros" && methhod === 'PUT') {
+
+        let body = '';
+        //.on sgfica que toda vez que essa requisição for feita inicia uma ação toda vez que ela é chamada
+        // data é o inicio e o fim é o end 
+        req.on('data', parte => {
+            body += parte
+        });
+
+
+
+        req.on('end', () => {
+            const livroAtualizado = JSON.parse(body);
+
+            livros = livros.map(livro => {
+                if (livro.id === livroAtualizado.id) {
+                    return livroAtualizado;
+                }
+                return livro
+            })
+            res.statusCode = 200; //sucesso
+
+
+            res.end(JSON.stringify({
+                mensagem: 'livro atualizado',
+                livros: livros
+            }))
+        });
+        return;
+    }
+
+    if (url === '/livros' && methhod === 'DELETE') {
+        let body = '';
+        //.on sgfica que toda vez que essa requisição for feita inicia uma ação toda vez que ela é chamada
+        // data é o inicio e o fim é o end 
+        req.on('data', parte => {
+            body += parte
+        });
+
+        req.on('end', () => {
+            const dados = JSON.parse(body);
+
+            livros = livros.filter(livro => 
+                livro.id !== dados.id 
+
+            )
+            console.log(livros)
+
+            console.log(dados)
+
+            res.statusCode = 200;
+
+            res.end(JSON.stringify({
+                mensagem: "livro deletado",
+                livros: livros
+            }))
+        })
+        return;
+    };
+
+    res.statusCode = 404;
+    res.end(JSON.stringify({
+        mensagem: "rota não encontrada"
+    }))
 
 })
 
